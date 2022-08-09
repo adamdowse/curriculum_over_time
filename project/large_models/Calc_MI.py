1380c80d47b5cd3c25f305c1f8d116a6af340ad3
Adam Dowse <adamdowse@hotmail.co.uk>
2022-08-09 11:52:48 +0000

    MP MI sorted but crashes with high num of images

--- project/large_models/Calc_MI.py
+++ project/large_models/Calc_MI.py
@@ -2,24 +2,19 @@
 import numpy as np
 import tensorflow_datasets as tfds
 import time
-from multiprocessing import Process, Value, Array
+from multiprocessing import Pool, Process, Value, Array
 
 #PLEASE BE CARFUL WITH THE HUGE ARRAYS YOU ARE ABOUT TO CREATE
-#collect dataset in same way as run main does via database
 
-
-def mutual_information_between_2(MI_mat,n_imgs,func_input):
+def mutual_information(func_in):
     """ Mutual information for joint histogram
     MI_mat : the shared mutual information to write to 
     i,j : the location to write to
     img1,img2 : the raveled images as lists
     """
-    #unpack
-    img1 = func_input[0]
-    img2 = func_input[1]
-    i    = func_input[2]
-    j    = func_input[3]
-    print(i[2])
+    #unpack vars
+    img1,img2,i,j = func_in
+    
 
     #convert the images into histograms
     hgram, x_edges, y_edges = np.histogram2d(img1,img2,bins=40)
@@ -31,33 +26,14 @@ def mutual_information_between_2(MI_mat,n_imgs,func_input):
     px_py = px[:, None] * py[None, :] # Broadcast to multiply marginals
     # Now we can do the calculation using the pxy, px_py 2D arrays
     nzs = pxy > 0 # Only non-zero pxy values contribute to the sum
-    MI_mat[i*n_imgs+j] = np.sum(pxy[nzs] * np.log(pxy[nzs] / px_py[nzs])) 
-
-def mutual_information_old(img1,img2):
-    """ Mutual information for joint histogram
-    MI_mat : the shared mutual information to write to 
-    i,j : the location to write to
-    img1,img2 : the raveled images as lists
-    """
-
-    #convert the images into histograms
-    hgram, x_edges, y_edges = np.histogram2d(img1,img2,bins=40)
-
-    # Convert bins counts to probability values
-    pxy = hgram / float(np.sum(hgram))
-    px = np.sum(pxy, axis=1) # marginal for x over y
-    py = np.sum(pxy, axis=0) # marginal for y over x
-    px_py = px[:, None] * py[None, :] # Broadcast to multiply marginals
-    # Now we can do the calculation using the pxy, px_py 2D arrays
-    nzs = pxy > 0 # Only non-zero pxy values contribute to the sum
-    return np.sum(pxy[nzs] * np.log(pxy[nzs] / px_py[nzs])) 
+    return (np.sum(pxy[nzs] * np.log(pxy[nzs] / px_py[nzs])),i,j )
 
 if __name__ == '__main__':
     #get data
     ds, ds_info = tfds.load('mnist',with_info=True,shuffle_files=False,as_supervised=True,split='train')
 
     #take the first n images to use as a proxi and test
-    n_imgs = 10
+    n_imgs = 6000
     img_list = []
     c = 0
     for img,label in ds:
@@ -76,55 +52,20 @@ if __name__ == '__main__':
             if i<j:
                 process_input.append((img_list[i],img_list[j],i,j))
     #process_input = [([img_0],[img_1],i,j)]
-    print(process_input[0][2])
 
+    #pool multiprocessing__
     t = time.time()
-    MI_mat = Array('i',[0 for x in range(n_imgs**2)])  #index via x*n_imgs+y
-
-    p = Process(target=mutual_information_between_2,args=(MI_mat,n_imgs,process_input))
-    p.start()
-    p.join()
-    print(time.time() - t)
-    print(MI_mat[:])
-
-    #no multiprocessing
-
-    t = time.time()
-    MI_matrix = np.zeros((n_imgs,n_imgs),dtype='int8')
-    for i,img1 in enumerate(img_list):
-        for j,img2 in enumerate(img_list):
-            if i<j:
-                MI_matrix[i,j] = mutual_information_old(img1,img2)
-
-    print(time.time() - t)
-    print(MI_matrix)
-    
-
-
-    #t = time.time()
-    #calc massive matrix of mutual information between images
-    #MI_matrix = np.zeros((n_imgs,n_imgs),dtype='int8')
-    #print('Mat size: ',MI_matrix.nbytes/(1*10**9),'Gbs')
-    
-    #c = 0
-    #for i,[img1,label1] in enumerate(ds):
-    #    for j,[img2,label2] in enumerate(ds): 
-    #        #check to see if the pair has been done before
-    #        if i<j:
-    #            MI_matrix[i,j] = mutual_information_between_2(img1.numpy().ravel(),img2.numpy().ravel())
-    #            c += 1
-    #        
-    #        if c == 100:
-    #            print((time.time() - t)/100)
-   # 
-   #     if i % 500 == 0:
-   #         print('i = ',i)
-
+    with Pool(3) as p:
+        map_out = p.map(mutual_information,process_input)
+    MI_mat = np.zeros((n_imgs,n_imgs))
+    for mi,i,j in map_out:
+        MI_mat[j,i] = mi
+    print(time.time()-t)
 
 
     #save the MI matrix to a csv to avoid recompute
   
     #TODO change this to a full address and put it in /MI/ so its git ignored
-    np.savetxt("MNIST_MI.csv", MI_matrix, delimiter=",")
+    np.savetxt("MI/MNIST_6000.csv", MI_mat, delimiter=",")
 
     #BMI formula np.sum(np.sum(MI_matrix))/(len(MI_matrix)*(len(MI_matrix)-1)/2)
